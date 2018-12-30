ed3d9643cdce77bc49ef86240795c06aacc68059
Jarod Mateus de Sousa Cavalcante <38592929+jarodsim@users.noreply.github.com>
2018-12-30 15:48:15 -0200

    alterando valores de login

--- js/login.js
+++ js/login.js
@@ -1,10 +1,12 @@
     function validarEmail(){
         var campoEmail = document.getElementById('campoemail');
         var campoSenha = document.getElementById('camposenha');
-        if(campoEmail == "jarodsim@gmail.com" && campoSenha == "123"){
+        if(campoEmail == "jarodsim@gmail.com" && campoSenha == 123){
             window.location.href = 'forum.html';
         }else{
             alert('email incorreto ou senha incoretos');
+            var cadastroopcao = document.getElementById('cadastrarNovoUser');
             document.getElementById('cadastrarNovoUser').innerHTML = 'Deseja se cadastrar?' ;
+            cadastroopcao.style.fontSize="18px";
         }
         }
